a39a3903912a20d9be35f055889cf41109e55e94
xq-asus-laptop\xq <xingquan6@gmail.com>
2016-09-07 23:35:04 -0400

    no message

---
+++ FrameWork/FrameWork/StockChart.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace FrameWork
+{
+    /// <summary>
+    /// Interaction logic for StockChart.xaml
+    /// </summary>
+    public partial class StockChart : Window
+    {
+        public StockChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
