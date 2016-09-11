9dc1b9fbd0313cb243cf0464ac41db4e24b43c44
guofengwu <exch_wu@yahoo.ca>
2016-09-11 19:04:17 -0400

    1.UI for create portfolio, cash transcation, sotock transcation 2. not connecting to database 3. need to solve the problem of difference between database table and UI data grid (not one to one, need to do calculation to get the right display value) 4. The code is under folder "Profile"

---
+++ Profile/Profile/CashTranscation.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Profile
+{
+    /// <summary>
+    /// Interaction logic for CashTranscation.xaml
+    /// </summary>
+    public partial class CashTranscation : Window
+    {
+        public Transcation trans = new Transcation();
+
+        public CashTranscation(string message)
+        {
+            InitializeComponent();
+            lblMessage.Content = message;
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void btSave_Click(object sender, RoutedEventArgs e)
+        {
+            //check if the date is correct
+            DateTime? date = dpkDate.SelectedDate;
+            if (date == null)
+            {
+              MessageBox.Show("Choose a Date!", "Input Error",
+              MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            trans.Date =(DateTime) date;
+
+            //check the amount is correct
+            double amt;
+            if(!double.TryParse(tbAmount.Text, out amt))
+                {
+              MessageBox.Show("please input a number! " + tbAmount.Text, "Input Error",
+              MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            trans.Price = amt;
+            trans.Notes = tbNotes.Text;
+
+            this.DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            dpkDate.Focus();
+        }
+    }
+}
---
+++ Profile/Profile/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile
+{
+    public enum TransType { Deposit, Withdraw, Buy, Sell }
+
+    public class Portfolio
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Transcation
+    {
+        public int portId { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public TransType Type { get; set; }
+        public DateTime Date { get; set; }
+        public int Share { get; set; }
+        public double Price { get; set; }
+        public string Notes { get; set; }
+    }
+
+}
