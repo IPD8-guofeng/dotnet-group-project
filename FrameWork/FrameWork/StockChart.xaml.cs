ec08bf523a74ba690ff517248fafca0a592b86d2
xq-asus-laptop\xq <xingquan6@gmail.com>
2016-09-13 07:47:42 -0400

    candle line, Volumn, channel line is done in the stock chart

--- FrameWork/FrameWork/StockChart.xaml.cs
+++ FrameWork/FrameWork/StockChart.xaml.cs
@@ -42,35 +42,68 @@ namespace FrameWork
                         this.ChartModel = model;
 
             */
-            List<StockPriceByDay> list = db.GetStockPriceByDayByTicker("A", DateTime.Parse("2016-01-01"));
-            var model = new PlotModel { Title = "A: start date" + "   2016-01-01" };
+            List<StockPriceByDay> list = db.GetStockPriceByDayByTicker("ABT", DateTime.Parse("2016-01-01"));
+            var model = new PlotModel { Title = "ABT: start date" + "   2016-01-01 \n" +"Candle line, Channel, Volumn"};
             model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
             model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
-            var highestPriceLine = new LineSeries { Title = " High price line", MarkerType = MarkerType.Diamond };
-            var lowestPriceLine = new LineSeries { Title = "Low price line", MarkerType = MarkerType.Diamond };
+            var highestPriceLine = new LineSeries { Title = " Channel", MarkerType = MarkerType.Diamond,  Color = OxyColors.Beige};
+            var lowestPriceLine = new LineSeries {  MarkerType = MarkerType.Diamond, Color = OxyColors.AliceBlue };
             double x = 0;
             foreach (StockPriceByDay s in list)
             {
                 //double x = s.PriceDate.Day + s.PriceDate.Month * 100 + (s.PriceDate.Year  % 100) * 10000;
                 //highestPriceLine.Points.Add(new DataPoint(DateTimeAxis.ToDouble(s.PriceDate), s.HighestPrice));
                 //lowestPriceLine.Points.Add(new DataPoint(DateTimeAxis.ToDouble(s.PriceDate), s.LowestPrice));
-                highestPriceLine.Points.Add(new DataPoint(x, s.HighestPrice));
-                lowestPriceLine.Points.Add(new DataPoint(x, s.LowestPrice));
+                highestPriceLine.Points.Add(new DataPoint(x, s.HighestPrice*1.05));
+                lowestPriceLine.Points.Add(new DataPoint(x, s.LowestPrice*0.95));
                 x++;
             }
-           // model.Series.Add(highestPriceLine);
-            //model.Series.Add(lowestPriceLine);
+            model.Series.Add(highestPriceLine);
+            model.Series.Add(lowestPriceLine);
 
             double y = 0;
             foreach (StockPriceByDay s in list)
             {
+
                 model.Series.Add(drawCandleLine(y,s.OpenPrice, s.HighestPrice, s.LowestPrice, s.ClosePrice));
+                model.Series.Add(drawTransAmountLine(y, s.TransAmount / 4000000));
                 y++;
             }
+
+            //var s1 = new BarSeries { StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            //s1.Items.Add(new BarItem { Value = 25 });
+            //s1.Items.Add(new BarItem { Value = 137 });
+            //s1.Items.Add(new BarItem { Value = 18 });
+            //s1.Items.Add(new BarItem { Value = 40 });
+
+            //var valueAxis = new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+            //model.Series.Add(s1);
+            //model.Axes.Add(valueAxis);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             this.ChartModel = model;
 
             //SetUpModel();
+
+
+
+
 
 
         }
@@ -87,7 +120,7 @@ namespace FrameWork
                 lowLine = closePrice;
                 highLine = openPrice;
             }
-            var rect = new LineSeries { MarkerType = MarkerType.None };
+            var rect = new LineSeries { MarkerType = MarkerType.None};
             rect.Points.Add(new DataPoint(startX, lowestPrice));
             rect.Points.Add(new DataPoint(startX, lowLine));
             rect.Points.Add(new DataPoint(startX - 0.5,lowLine));
@@ -100,6 +133,19 @@ namespace FrameWork
             rect.Points.Add(new DataPoint(startX, lowLine));
             return rect;
         }
+
+        private LineSeries drawTransAmountLine(double startX, double startY)
+        {
+            var rect = new LineSeries { MarkerType = MarkerType.None, Color = OxyColors.DarkOrange };
+            rect.Points.Add(new DataPoint(startX, 30));
+            rect.Points.Add(new DataPoint(startX, 30+startY));
+            return rect;
+        }
+
+
+
+
+
         //private void SetUpModel()
         //{
         //    var model = new PlotModel { Title = "A: start date" + "   2015-01-01" };
