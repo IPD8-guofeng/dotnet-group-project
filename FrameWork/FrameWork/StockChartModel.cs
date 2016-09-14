b55e8a238db0078d08a2b1dd26df7bfc29fe4787
213-18\student <xingquan6@gmail.com>
2016-09-14 13:50:44 -0400

    update the chart window, it can switch to anorther stock chart

--- FrameWork/FrameWork/StockChartModel.cs
+++ FrameWork/FrameWork/StockChartModel.cs
@@ -7,43 +7,68 @@ using OxyPlot;
 using OxyPlot.Series;
 using OxyPlot.Axes;
 using OxyPlot.Annotations;
+using System.ComponentModel;
 
 namespace FrameWork
 {
-    class StockChartModel
+    class StockChartModel : INotifyPropertyChanged
     {
         Database db = new Database();
-        public PlotModel ChartModel { get; set; }
-        public StockChartModel(string ticker)
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private PlotModel _ChartModel;
+        public PlotModel ChartModel
         {
-            //string ticker = "A";
-            DateTime startDate = DateTime.Parse("01/01/2016");
-            DateTime endDate = DateTime.Now;
-            List<StockPriceByDay> list = db.GetStockPriceByDayByTicker(ticker, startDate);
-            string title = "Stock "+ ticker + ": From " + startDate.ToShortDateString() + " to " + endDate.ToShortDateString() + "\n" + "Candle line, Channel, Volumn";
-            var model = new PlotModel { Title = title  };
-            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
-            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
-            var highestPriceLine = new LineSeries { MarkerType = MarkerType.Diamond, Color = OxyColors.Beige };
-            var lowestPriceLine = new LineSeries { Title = " Channel", MarkerType = MarkerType.Diamond, Color = OxyColors.AliceBlue };
-
-            double volumnUnit = (list[0].TransAmount / list[0].HighestPrice) * 10;
-            double x = 0;
-            double y = list[0].LowestPrice * 0.7;
-            foreach (StockPriceByDay s in list)
+            get { return _ChartModel; }
+            set
             {
-                highestPriceLine.Points.Add(new DataPoint(x, s.HighestPrice * 1.05));
-                lowestPriceLine.Points.Add(new DataPoint(x, s.LowestPrice * 0.95));
-                model.Series.Add(drawCandleLine(x, s.OpenPrice, s.HighestPrice, s.LowestPrice, s.ClosePrice));  // draw one single candle
-                double volumn = s.TransAmount / volumnUnit;
-                if (volumn > (s.HighestPrice - volumnUnit)) volumn /= 2;
-                model.Series.Add(drawTransAmountLine(x, y , volumn));  // draw one single volumn
-                x++;
+                _ChartModel = value;
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null)
+                {
+                    handler(this, new PropertyChangedEventArgs("ChartModel"));
+                }
             }
-            model.Series.Add(highestPriceLine); // draw the channel top line
-            model.Series.Add(lowestPriceLine); // draw the channel bottom line
+        }
+        public StockChartModel(string ticker, string startDateStr, string endDateStr)
+        {
+            SetStockTicker(ticker, startDateStr, endDateStr);
+        }
+        public void SetStockTicker(string ticker, string startDateStr, string endDateStr)
+        {
+            if (ticker != null && ticker != "")
+            {
+                DateTime startDate = DateTime.Parse(startDateStr);
+                DateTime endDate = DateTime.Now;
+                List<StockPriceByDay> list = db.GetStockPriceByDayByTicker(ticker, startDate);
+                string title = "Stock " + ticker + ": From " + startDate.ToShortDateString() + " to " + endDate.ToShortDateString() + "\n" + "Candle line, Channel, Volumn";
 
-            this.ChartModel = model;
+                var model = new PlotModel { Title = title };
+                model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+                model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+                var highestPriceLine = new LineSeries { MarkerType = MarkerType.Diamond, Color = OxyColors.Beige };
+                var lowestPriceLine = new LineSeries { Title = " Channel", MarkerType = MarkerType.Diamond, Color = OxyColors.AliceBlue };
+
+                double volumnUnit = (list[0].TransAmount / list[0].HighestPrice) * 10;
+                double x = 0;
+                double y = list[0].LowestPrice * 0.7;
+                foreach (StockPriceByDay s in list)
+                {
+                    highestPriceLine.Points.Add(new DataPoint(x, s.HighestPrice * 1.05));
+                    lowestPriceLine.Points.Add(new DataPoint(x, s.LowestPrice * 0.95));
+                    model.Series.Add(drawCandleLine(x, s.OpenPrice, s.HighestPrice, s.LowestPrice, s.ClosePrice));  // draw one single candle
+                    double volumn = s.TransAmount / volumnUnit;
+                    if (volumn > (s.HighestPrice - volumnUnit)) volumn /= 2;
+                    model.Series.Add(drawTransAmountLine(x, y, volumn));  // draw one single volumn
+                    x++;
+                }
+                model.Series.Add(highestPriceLine); // draw the channel top line
+                model.Series.Add(lowestPriceLine); // draw the channel bottom line
+
+                this.ChartModel = model;
+                ChartModel.InvalidatePlot(true);
+            }
         }
 
         private LineSeries drawCandleLine(double startX, double openPrice, double highestPrice, double lowestPrice, double closePrice)
@@ -77,7 +102,7 @@ namespace FrameWork
         {
             var volumnLine = new LineSeries { MarkerType = MarkerType.None, Color = OxyColors.DarkOrange };
             volumnLine.Points.Add(new DataPoint(startX, startY));
-            volumnLine.Points.Add(new DataPoint(startX, startY+volumn));
+            volumnLine.Points.Add(new DataPoint(startX, startY + volumn));
             return volumnLine;
         }
     }
---
+++ TradeStock_qrySellStock.sql
@@ -0,0 +1,13 @@
+﻿SELECT StockTicker, ((SELECT SUM(Quantity) AS sumBuy FROM [Transaction] WHERE ActionType = 1 GROUP BY StockTicker, ActionType) - (SELECT SUM(Quantity) AS sumSell FROM[Transaction] WHERE ActionType = 2 GROUP BY StockTicker, ActionType)) AS StockQuantity, ((SELECT SUM(Quantity*Price) AS sumBuyCost FROM [Transaction] WHERE ActionType = 1 GROUP BY StockTicker, ActionType) - (SELECT SUM(Quantity*Price) AS sumSellMoney FROM[Transaction] WHERE ActionType = 2 GROUP BY StockTicker, ActionType)) AS sumTotalCost FROM[Transaction] GROUP BY StockTicker
+
+
+SELECT StockTicker, (( SELECT SUM(Quantity*Price) AS sumBuyCost FROM [Transaction] WHERE ActionType = 1 GROUP BY StockTicker) - (SELECT SUM(Quantity*Price) AS sumSellMoney FROM[Transaction] WHERE ActionType = 2 GROUP BY StockTicker)) AS sumCost FROM[Transaction] GROUP BY StockTicker
+
+SELECT StockTicker, 
+(
+( SELECT SUM(Quantity) AS sumBuyQty FROM [Transaction] WHERE ActionType = 1 GROUP BY StockTicker) 
+- (SELECT SUM(Quantity) AS sumSellQty FROM [Transaction] WHERE ActionType = 2 GROUP BY StockTicker)
+) AS sumQty 
+FROM[Transaction] 
+GROUP BY StockTicker
+
